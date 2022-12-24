cd69ae482254100b4a420c1b6f31b2e3cfbd62b8
scriptPilot <dennis@scheffel.de>
2022-12-24 22:26:36 +0100

    ESLint

--- source/frontend/main.js
+++ source/frontend/main.js
@@ -1,12 +1,10 @@
 import { createApp } from 'vue'
 import App from './components/App.vue'
-
-const app = createApp(App)
-
 import Framework7 from 'framework7/lite'
 import Framework7Vue from 'framework7-vue'
-Framework7.use(Framework7Vue)
 import '../../node_modules/framework7/framework7-bundle.min.css'
 
-app.mount('#app')
+const app = createApp(App)
+Framework7.use(Framework7Vue)
 
+app.mount('#app')
