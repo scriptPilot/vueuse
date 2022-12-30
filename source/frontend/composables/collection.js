c4ba2a59b8ba1925c76e08c04dff45531298a3e4
scriptPilot <dennis@scheffel.de>
2022-12-30 12:57:20 +0100

    use LocalStorage for Collection

--- source/frontend/composables/collection.js
+++ source/frontend/composables/collection.js
@@ -1,34 +1,21 @@
-import { reactive, readonly } from 'vue'
+import { reactive } from 'vue'
+import { useLocalStorage } from './LocalStorage'
 import { v4 as uuid } from 'uuid'
 
 export function useCollection ({ localStorageKey = null, primaryKey = '$key' }) {
 
-  const docs = reactive([])
+  let docs
 
-  function writeToLocalStorage () {
-    if (localStorageKey) {
-      window.localStorage.setItem(localStorageKey, JSON.stringify(docs))
-      console.debug('Write to LocalStorage')
-    }
-  }
-
-  function readFromLocalStorage () {
-    if (localStorageKey) {
-      try {
-        const localStorageValue = window.localStorage.getItem(localStorageKey)
-        const localStorageJson = JSON.parse(localStorageValue)
-        if (Array.isArray(localStorageJson)) {
-          localStorageJson.forEach(object => docs.push(object))
-          console.debug('Read from LocalStorage')
-        }
-      } catch {}
-    }
+  if (localStorageKey) {
+    const { state } = useLocalStorage({ localStorageKey, defaultValue: [] })
+    docs = state
+  } else {
+    docs = reactive([])
   }
 
   function addDoc ({ doc }) {
     if (!doc[primaryKey]) doc[primaryKey] = uuid()
     docs.push({ ...doc })
-    writeToLocalStorage()
   }
 
   function updateDoc ({ key, updates }) {
@@ -37,7 +24,6 @@ export function useCollection ({ localStorageKey = null, primaryKey = '$key' }) 
         docs.splice(n, 1, { ...docs[n], ...updates })
       }
     }
-    writeToLocalStorage()
   }
 
   function removeDoc ({ key }) {
@@ -46,7 +32,6 @@ export function useCollection ({ localStorageKey = null, primaryKey = '$key' }) 
         docs.splice(n, 1)
       }
     }
-    writeToLocalStorage()
   }
 
   function setDocs ({ docs: newDocs }) {
@@ -54,10 +39,7 @@ export function useCollection ({ localStorageKey = null, primaryKey = '$key' }) 
       if (!newDocs[n][primaryKey]) newDocs[n][primaryKey] = uuid()
     }
     docs.splice(0, docs.length, ...newDocs)
-    writeToLocalStorage()
   }
-
-  if (localStorageKey) readFromLocalStorage()
 
   return {
     documents: readonly(docs),
